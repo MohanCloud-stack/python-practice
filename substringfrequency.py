dc542ecd894ac7ac2fa145bfe07fd1813827ac98
SAI MOHAN <45478292+MohanCloud-stack@users.noreply.github.com>
2023-04-16 07:53:08 +0530

    Create substringfrequency.py

---
+++ substringfrequency.py
@@ -0,0 +1,8 @@
+print("enter the numbers including the strings")
+str=input().split(" ")
+print("enter the string that is present inside the array")
+checker=input()
+if checker in str:
+    print("yes")
+else:
+    print("no")
