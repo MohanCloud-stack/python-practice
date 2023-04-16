e5c20bfaee9b96bd81d26b44dc6ac86ac5b842b1
SAI MOHAN <45478292+MohanCloud-stack@users.noreply.github.com>
2023-04-16 06:30:23 +0530

    Update LongestCommonprefix.py

--- LongestCommonprefix.py
+++ LongestCommonprefix.py
@@ -1,12 +1,12 @@
-def longpossiblestring(str):
-    temp=[]
-    if(len(str)) > 0:
-        #str.sort(key=len)
-        first,last=str[0],str[-1]
-        for i in  range(len(first)):
-            if(len(last) > i and first[i] == last[i]):
-                temp.append(first[i])
-            else:
-                return " ".join(temp)
-    return " ".join(temp)
-print(longpossiblestring(["abab","aba","abc"]))
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        temp=[]
+        if(len(strs)) > 0:
+            strs1=sorted(strs)
+            first,last=strs1[0],strs1[-1]
+            for i in  range(len(first)):
+                if(len(last) > i and last[i] == first[i]):
+                    temp.append(first[i])
+                else:
+                    return "".join(temp)
+        return "".join(temp)
