179abbb7f98f8e5d9549bf148eae5c0d38c4507b
SAI MOHAN <45478292+MohanCloud-stack@users.noreply.github.com>
2023-04-18 15:22:58 +0530

    Update maximumoccuringsubstring.py

--- maximumoccuringsubstring.py
+++ maximumoccuringsubstring.py
@@ -1,6 +1,15 @@
-str = "gfghsisbjknlmkesbestgfgsdcngfgcsdjnisdjnlbestdjsklgfgcdsbestbnjdsgfgdbhisbhsbestdkgfgb"
-l1 = ['gfg', 'is', 'best']
+str="gfghsisbjknlmkesbestgfgsdcngfgcsdjnisdjnlbestdjsklgfgcdsbestbnjdsgfgdbhisbhsbestdkgfgbbestbestbestbest"
+l1=['gfg','is','best']
+minocc=1
+count=0
 obj={}
+item=" "
 for i in l1:
-    obj[i]=str.count(i)
-print(obj)
+    if i in str:
+        res=str.count(i)
+        obj[i]=res
+        count+=1
+    if (obj[i]>minocc):
+        minocc=obj[i]
+        item=i
+print(item)
